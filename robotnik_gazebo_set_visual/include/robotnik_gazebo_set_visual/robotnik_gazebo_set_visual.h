9ffbc111f8252b57263befc090d736320d28836f
rjuliaros <rjulia@robotnik.es>
2021-03-11 17:46:18 +0100

    set_visual: refactored code, added loggers for debug and removed unused parts

--- robotnik_gazebo_set_visual/include/robotnik_gazebo_set_visual/robotnik_gazebo_set_visual.h
+++ robotnik_gazebo_set_visual/include/robotnik_gazebo_set_visual/robotnik_gazebo_set_visual.h
@@ -20,131 +20,52 @@
 #define ROBOTNIK_GAZEBO_SET_VISUAL_HH
 
 #include <string>
-
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
-
 #include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-
-#include <ros/callback_queue.h>
 #include <ros/advertise_options.h>
-
-#include <tf/transform_broadcaster.h>
-#include <std_srvs/SetBool.h>
-#include <std_srvs/Empty.h>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/TransportTypes.hh>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/common/Time.hh>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/Events.hh>
-#include <gazebo/rendering/RenderingIface.hh>
 #include <gazebo/rendering/Scene.hh>
 #include <gazebo_msgs/SetLightProperties.h>
 
-#include <gazebo_plugins/PubQueue.h>
-
 namespace gazebo
 {
-  struct ColorRGBA
-  {
-    /// \brief The name of the color.
-    public: std::string color_name;
-    /// \brief The rgba representation of the color.
-    public: ignition::math::Color color_code;
-  };
-
   class RobotnikGazeboSetVisual : public ModelPlugin
   {
-    /// \brief Constructor
-    public: RobotnikGazeboSetVisual();
+    public: 
+      RobotnikGazeboSetVisual();                                    //Constructor
+      virtual ~RobotnikGazeboSetVisual();                           //Destructor
+      void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);   //Parameters loader
 
-    /// \brief Destructor
-    public: virtual ~RobotnikGazeboSetVisual();
+    protected:
+      virtual void OnUpdate();
+      gazebo::msgs::Visual SetColor();
+      bool setColorSrvCallback(gazebo_msgs::SetLightProperties::Request& request, gazebo_msgs::SetLightProperties::Response& response);
 
-    /// \brief Load the controller
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
-
-    /// \brief Update the controller
-    public: void OnUpdate();
-
-    /// \brief Update the controller  //TODO: Delete this method
-    protected: virtual void UpdateChild();
-
-    //protected: bool setColorSrvCallback(ignition::math::Color &color_code);
-    
-    protected: ignition::math::Color color_;
-    protected: gazebo::msgs::Visual SetColor();
-    protected: bool readVisual();
-    protected: std::vector<std::string> visuals_;
-    public: int currentBlockIndex;
-
-    private: physics::WorldPtr world_;
-    private: physics::ModelPtr model_;
-
-    /// \brief The parent Model
-    private: physics::LinkPtr link_;
-
-    /// \brief The body of the frame to display pose, twist
-    private: physics::LinkPtr reference_link_;
-
-
-    /// \brief pointer to ros node
-    private: ros::NodeHandle* rosnode_;
-    private: ros::Publisher pub_;
-    private: PubQueue<nav_msgs::Odometry>::Ptr pub_Queue;
-    private: boost::shared_ptr<tf::TransformBroadcaster> transform_broadcaster_;
-
-
-    /// \brief ros message
-    private: nav_msgs::Odometry pose_msg_;
-
-    /// \brief store bodyname
-    private: std::string link_name_;
-
-    /// \brief store bodyname
-    private: std::string visual_name_;
-
-    /// \brief mutex to lock access to fields used in message callbacks
-    private: boost::mutex lock;
-
-    // rate control
-    private: double update_rate_;
-
-    private: common::Time last_time_;
-
-    /// \brief for setting ROS name space
-    private: std::string robot_namespace_;
-
-    private: ros::CallbackQueue ground_truth_odom_queue_;
-    private: void QueueThread();
-    private: boost::thread callback_queue_thread_;
-
-    // Pointer to the update event connection
-    private: event::ConnectionPtr update_connection_;
-
-    private: unsigned int seed;
-
-    // ros publish multi queue, prevents publish() blocking
-    private: PubMultiQueue pmq;
-
-    private: gazebo::transport::NodePtr node_;
-    private: gazebo::transport::PublisherPtr pub_visual_;
-    private: gazebo::event::ConnectionPtr updateConnection;
-    private: gazebo::rendering::ScenePtr scene_ptr;
-    private: gazebo::rendering::VisualPtr box_ptr ;
-    private: size_t iterrations_ = 0;       
-    public: float r,g,b,a;
-
-    //private: bool OnServiceCallback(std_srvs::Empty::Request &req,
-    // std_srvs::Empty::Response &res)
-    private: ros::NodeHandle nh_;
-    private: ros::ServiceServer set_color_srv_;
-    private: bool setColorSrvCallback(gazebo_msgs::SetLightProperties::Request& request, gazebo_msgs::SetLightProperties::Response& response);
-    private: std_msgs::ColorRGBA color;
-    std::vector<std::string> scoped_visuals;
-
+    private: 
+      std::vector<std::string> visuals_;
+      physics::WorldPtr world_;
+      physics::ModelPtr model_;
+      physics::LinkPtr link_;
+      ros::NodeHandle* rosnode_;
+      std::string link_name_;
+      std::string visual_name_;
+      common::Time last_time_;
+      std::string robot_namespace_;
+      event::ConnectionPtr update_connection_;  // Pointer to the update event connection
+      unsigned int seed;
+      gazebo::transport::NodePtr node_;
+      gazebo::transport::PublisherPtr pub_visual_;
+      gazebo::event::ConnectionPtr updateConnection;
+      float r,g,b,a;
+      ros::NodeHandle nh_;
+      ros::ServiceServer set_color_srv_;
+      std_msgs::ColorRGBA color;
+      std::vector<std::string> scoped_visuals;
+      std::string visual_id_2change;
   };
 }
 #endif
--- robotnik_gazebo_set_visual/src/robotnik_gazebo_set_visual.cpp
+++ robotnik_gazebo_set_visual/src/robotnik_gazebo_set_visual.cpp
@@ -37,16 +37,14 @@ RobotnikGazeboSetVisual::~RobotnikGazeboSetVisual()
   this->update_connection_.reset();
   // Finalize the controller
   this->rosnode_->shutdown();
-  this->ground_truth_odom_queue_.clear();
-  this->ground_truth_odom_queue_.disable();
-  this->callback_queue_thread_.join();
   delete this->rosnode_;
 }
 
 // Load the controller
 void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
-  ROS_WARN("Set Visual Charged!");
+  ROS_INFO_NAMED("Set_visual", "Loading robotnik_gazebo_set_visual");
+  
   // Get the world name.
   this->world_ = _parent->GetWorld();
   this->model_ = _parent;
@@ -56,11 +54,11 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
   if (_sdf->HasElement("robotNamespace"))
   {
     this->robot_namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
-    //ROS_INFO_NAMED("set_visual", "robotnik_gazebo_set_visual robot_namespace: %s", this->robot_namespace_.c_str());
+    ROS_DEBUG_NAMED("set_visual", "robotnik_gazebo_set_visual robot_namespace: %s", this->robot_namespace_.c_str());
   }
   else
   {
-    //ROS_INFO_NAMED("set_visual", "robotnik_gazebo_set_visual has no namespace");
+    ROS_DEBUG_NAMED("set_visual", "robotnik_gazebo_set_visual has no namespace");
   }
 
   //Reads and checks the existance of the link specified on the modelName label 
@@ -83,18 +81,9 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
     return;
   }
 
-  //Reads and checks the existance of the link specified on the modelName label 
-  /*if (!_sdf->HasElement("visualName"))
-  {
-    ROS_FATAL_NAMED("set_visual", "robotnik_gazebo_set_visual plugin missing <visualName>, cannot proceed");
-    return;
-  }
-  else
-  {
-    this->visual_name_ = _sdf->GetElement("visualName")->Get<std::string>();
-    ROS_INFO_NAMED("set_visual", "robotnik_gazebo_set_visual plugin loaded for %s and visual %s", this->link_name_.c_str(), this->visual_name_.c_str());
-  }*/
-  
+  //Reads visual tags, each tag must contain a visualName label 
+  //and this label represents the name of the visual inside the link. 
+  //It is important to know that the visuals must be inside part of the same link
   int i = 0;
   if (_sdf->HasElement("visual"))
   {
@@ -103,12 +92,12 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
     while (sdfVisual)
     {
       std::string visual_sdf;
-      ROS_WARN("Has element %d", i);
       // name
       if (sdfVisual->HasElement("visualName"))
       {
         visual_sdf = sdfVisual->Get<std::string>("visualName");
         i++;
+        ROS_DEBUG_NAMED("set_visual", "%d: %s readed", i, visual_sdf.c_str());
       }
       else
       {
@@ -117,6 +106,7 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
       this->visuals_.push_back(visual_sdf);
       sdfVisual = sdfVisual->GetNextElement("visual");
     }
+    ROS_DEBUG_NAMED("set_visual", "'visualName' readed succesfully!");
   }
   else
   {
@@ -132,6 +122,7 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
     return;
   }
 
+  //Gets the scope for setting the full name of the visuals
   physics::LinkPtr link_ = this->model_->GetLink(link_name_);
   std::string scoped_name = link_->GetScopedName().c_str();
 
@@ -149,88 +140,73 @@ void RobotnikGazeboSetVisual::Load(physics::ModelPtr _parent, sdf::ElementPtr _s
   set_color_srv_ = nh_.advertiseService("/set_visual_raquel", &RobotnikGazeboSetVisual::setColorSrvCallback, this);
 }
 
-// Update the controller
-void RobotnikGazeboSetVisual::UpdateChild()
-{
-  if (!this->link_)
-    return;
-
-#if GAZEBO_MAJOR_VERSION >= 8
-  common::Time cur_time = this->world_->SimTime();
-#else
-  common::Time cur_time = this->world_->GetSimTime();
-#endif
-
-  if (cur_time < this->last_time_)
-  {
-      ROS_WARN_NAMED("set_visual", "Negative update time difference detected.");
-      this->last_time_ = cur_time;
-  }
-      // save last time stamp
-      this->last_time_ = cur_time;
-}
-
-// Put laser data to the interface
-void RobotnikGazeboSetVisual::QueueThread()
-{
-  static const double timeout = 0.01;
-
-  while (this->rosnode_->ok())
-  {
-    this->ground_truth_odom_queue_.callAvailable(ros::WallDuration(timeout));
-  }
-}
-
+// Update the controller, virtual method
 void RobotnikGazeboSetVisual::OnUpdate()
 {
-  //ROS_WARN("Updating gazebo_set_visual");
+  //ROS_WARN_NAMED("set_visual", "Updating gazebo_set_visual");
 }
 
 gazebo::msgs::Visual RobotnikGazeboSetVisual::SetColor()
 {
-  gazebo::msgs::Visual visualMsg = link_->GetVisualMessage(scoped_visuals[0]);
-
-  visualMsg.set_name(link_->GetScopedName());
-  //ROS_WARN("%s", link_->GetScopedName().c_str());
-
-  visualMsg.set_parent_name(this->model_->GetScopedName());
-  //ROS_ERROR("%s", this->model_->GetScopedName().c_str());
-
-  // Initiate material
-  if ((!visualMsg.has_material()) || visualMsg.mutable_material() == NULL)
+  int i = 0;
+  //Check the existance of the visual received
+  while (visuals_[i] != visual_id_2change && i < visuals_.size())
   {
-    ROS_ERROR_NAMED("set_visual","%s does not contain material label, it can be due to the combination does not exist", link_->GetScopedName().c_str());
-    gazebo::msgs::Material *materialMsg = new gazebo::msgs::Material;
-    visualMsg.set_allocated_material(materialMsg);
+    i++;
+  }
+  
+  gazebo::msgs::Visual visualMsg;
+
+  if(i!=visuals_.size())
+  {
+    visualMsg = link_->GetVisualMessage(scoped_visuals[i]);
+
+    visualMsg.set_name(link_->GetScopedName());
+    //ROS_WARN("%s", link_->GetScopedName().c_str());
+  
+    visualMsg.set_parent_name(this->model_->GetScopedName());
+    //ROS_ERROR("%s", this->model_->GetScopedName().c_str());
+  
+    // Initiate material
+    if ((!visualMsg.has_material()) || visualMsg.mutable_material() == NULL)
+    {
+      ROS_ERROR_NAMED("set_visual","%s does not contain material label, maybe this visual is not defined on the urdf with a valid material tag.", link_->GetScopedName().c_str());
+      gazebo::msgs::Material *materialMsg = new gazebo::msgs::Material;
+      visualMsg.set_allocated_material(materialMsg);
+    }
+    else
+    {
+
+      ROS_DEBUG_NAMED("set_visual", "%s contains material label, creating the visualMsg", link_->GetScopedName().c_str());
+  
+    // Set color
+    // gazebo::msgs::Color is deprecated for Gazebo > 7  
+  #if GAZEBO_MAJOR_VERSION >= 8
+      ignition::math::Color newColor(color.r, color.g, color.b, color.a);
+  #else
+      gazebo::msgs::Color newColor(color.r, color.g, color.b, color.a); //TODO: No tested, deprecated.
+  #endif
+  
+      gazebo::msgs::Color *colorMsg = new gazebo::msgs::Color(gazebo::msgs::Convert(newColor));
+      gazebo::msgs::Color *diffuseMsg = new gazebo::msgs::Color(*colorMsg);
+      gazebo::msgs::Material *materialMsg = visualMsg.mutable_material();
+      if (materialMsg->has_ambient())
+      {
+        materialMsg->clear_ambient();
+      }
+      materialMsg->set_allocated_ambient(colorMsg);
+      if (materialMsg->has_diffuse())
+      {
+        materialMsg->clear_diffuse();
+      }
+      materialMsg->set_allocated_diffuse(diffuseMsg);
+    }
   }
   else
   {
-    ROS_DEBUG_NAMED("set_visual", "%s contains material label", link_->GetScopedName().c_str());
+    ROS_ERROR_NAMED("set_visual", "No matching visual. Did you forget to add %s as a visual on the plugin?", visual_id_2change.c_str());
   }
-
-  
-  // Set color
-  // gazebo::msgs::Color is deprecated for Gazebo > 7  
-#if GAZEBO_MAJOR_VERSION >= 8
-  ignition::math::Color newColor(color.r, color.g, color.b, color.a);
-#else
-  gazebo::msgs::Color newColor(r, g, b, a);
-#endif
-
-  gazebo::msgs::Color *colorMsg = new gazebo::msgs::Color(gazebo::msgs::Convert(newColor));
-  gazebo::msgs::Color *diffuseMsg = new gazebo::msgs::Color(*colorMsg);
-  gazebo::msgs::Material *materialMsg = visualMsg.mutable_material();
-  if (materialMsg->has_ambient())
-  {
-    materialMsg->clear_ambient();
-  }
-  materialMsg->set_allocated_ambient(colorMsg);
-  if (materialMsg->has_diffuse())
-  {
-    materialMsg->clear_diffuse();
-  }
-  materialMsg->set_allocated_diffuse(diffuseMsg);
-  //return;
+ 
   return visualMsg;  
 }
 
@@ -238,7 +214,10 @@ bool RobotnikGazeboSetVisual::setColorSrvCallback(gazebo_msgs::SetLightPropertie
                                               gazebo_msgs::SetLightProperties::Response& response)
 {
   color = request.diffuse;
+  visual_id_2change = request.light_name;
 
+  ROS_DEBUG_NAMED("set_visual", "Received a petition to change the color of %s to color.r: %f, color.g: %f, color.b: %f, color.a: %f", visual_id_2change.c_str(), color.r, color.g, color.b, color.a);
+  
   pub_visual_->Publish(SetColor());
   response.success = true;
   return true;
